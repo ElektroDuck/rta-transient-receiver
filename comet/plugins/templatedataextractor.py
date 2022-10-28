9ac27fbfd8eba30f96a7200b6a9b01a175d872ec
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-10-28 15:18:29 +0200

    created abstract class as template for the data estractors

---
+++ comet/plugins/templatedataextractor.py
@@ -0,0 +1,97 @@
+
+import os
+import re
+import json
+import math
+import requests
+import numpy as np
+import comet.log as log
+import voeventparse as vp
+from tokenize import group
+from datetime import datetime
+from datetime import datetime
+from astropy.time import Time
+from astropy import units as u
+import lxml.etree as ElementTree
+from comet.utility import voevent
+from astropy.coordinates import SkyCoord
+from comet.utility.xml import xml_document
+from ligo.skymap.io.fits import read_sky_map
+from ligo.skymap.postprocess.contour import contour as ligo_contour
+
+
+class templatedataextractor(object):
+    
+    
+    def __init__(self, datasource) -> None:
+        self.datasource = datasource
+
+    def extract(self, voevent):
+        is_ste = self.is_ste(self, voevent)
+        instrument_id = self.get_instrumentID(self, voevent)
+        trigger_id = self.get_triggerID(self, voevent)
+        packet_type = self.get_packet_type(self, voevent)
+        time = self.get_time_from_voevent(self, voevent)
+        network_id = self.get_networkID(self, voevent)
+        l, b = self.get_l_b(self, voevent)
+        position_error = self.get_position_error(self, voevent)
+        notice = vp.prettystr(self.voevent)
+        configuration = self.get_configuration(self, voevent)
+        url = self.get_url(self, voevent)
+        contour = self.get_contour(self, voevent)
+        ligo_attributes = self.get_ligo_attributes(self, voevent)
+        
+        #static fields that probably should be not static 
+        name = ""
+        seqNum = -1
+        tstart = 0
+        tstop = 0
+        last = 1
+
+        #here need to be create a new class that store the previusly 
+        #extracted data and return it
+        
+
+    def is_ste(self, voevent):
+        raise NotImplementedError
+
+    def get_instrumentID(self, voevent):
+        raise NotImplementedError
+
+    def get_triggerID(self, voevent):
+        raise NotImplementedError
+
+    def get_packet_type(args, voevent):
+        raise NotImplementedError
+
+    def get_time_from_voevent(self, voevent):
+        iso_time = self.voevent.WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Time.TimeInstant.ISOTime.text
+        t = Time(iso_time, format="fits", scale="utc")
+        return np.round(t.unix - 1072915200), t.fits
+
+    def get_networkID(self, voevent):
+        raise NotImplementedError
+
+    def get_l_b(self, voevent):
+        raise NotImplementedError
+
+    def get_position_error(self, voevent):
+        raise NotImplementedError
+
+    def get_configuration(self, voevent):
+        raise NotImplementedError
+
+    def get_url(self, voevent):
+        raise NotImplementedError
+
+    def get_contour(self, voevent):
+        raise NotImplementedError
+
+    def get_ligo_attributes(self, voevent):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return "class for data extraction from: %s"% (self.datasource)
+
+    def __str__(self):
+        return "class for data extraction from: %s"% (self.datasource)
