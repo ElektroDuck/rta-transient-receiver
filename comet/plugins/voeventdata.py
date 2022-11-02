9139582e4e7e4fe2a7e3278990f0c2e63f6f2caf
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-11-02 10:37:30 +0100

    added field datasource and a __str__ method

--- comet/plugins/voeventdata.py
+++ comet/plugins/voeventdata.py
@@ -1,8 +1,9 @@
 class Voeventdata(object):
-    def __init__(self, is_ste, instrument_id, trigger_id,
+    def __init__(self, datasource, is_ste, instrument_id, trigger_id,
                     packet_type, time, network_id, l, b, position_error,
                     notice, configuration, url, contour, ligo_attributes,
                     name, seqNum, tstart, tstop, last) -> None:
+        self.datasource = datasource
         self.is_ste = is_ste
         self.instrument_id = instrument_id
         self.trigger_id = trigger_id
@@ -22,3 +23,6 @@ class Voeventdata(object):
         self.tstart = tstart
         self.tstop = tstop
         self.last = last
+
+    def __str__(self):
+        return f"Voevent\nIntrumentID: {self.instrument_id}, name:{self.name}, seqNum {self.seqNum}, triggerid: {self.trigger_id}, packetType: {self.packet_type},time: {self.time}, l: {self.l}, b: {self.b}, contour: {self.contour}, url: {self.url}"
